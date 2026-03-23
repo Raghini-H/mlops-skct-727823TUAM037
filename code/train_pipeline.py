580e642ca000f2e1c0dc5666f30ba7da2f016350
RaghiniH <727823tuam037@skct.edu.in>
2026-03-23 16:18:09 +0530

    Updated files

--- code/train_pipeline.py
+++ code/train_pipeline.py
@@ -14,9 +14,6 @@ from sklearn.metrics import classification_report, f1_score
 
 print("Name : Raghini H | RollNo: 727823TUAM037 | Timestamp:", datetime.now())
 
-# -----------------------------
-# LOAD DATA
-# -----------------------------
 data = pd.read_csv(
     r"C:\College\Semester 6\MLOPS\Assignment\ML\data\secom.data",
     sep=r'\s+',
@@ -29,47 +26,30 @@ labels = pd.read_csv(
     header=None
 )
 
-# -----------------------------
-# SPLIT FEATURES & TARGET
-# -----------------------------
 X = data
 y = labels[0]
 
-# Convert labels (IMPORTANT: anomaly → binary classification)
 y = y.replace({1: 0, -1: 1})
 
 print("\nClass distribution:")
 print(y.value_counts())
 
-# Fix column names (IMPORTANT)
 X.columns = X.columns.astype(str)
 
-# -----------------------------
-# PREPROCESSING (ONLY X)
-# -----------------------------
 imputer = SimpleImputer(strategy='mean')
 X = imputer.fit_transform(X)
 
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-# -----------------------------
-# TRAIN TEST SPLIT
-# -----------------------------
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-# -----------------------------
-# MODEL TRAINING
-# -----------------------------
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
 model.fit(X_train, y_train)
 
-# -----------------------------
-# PREDICTION & EVALUATION
-# -----------------------------
 y_pred = model.predict(X_test)
 
 f1 = f1_score(y_test, y_pred)
@@ -78,15 +58,10 @@ print("\nF1 Score:", round(f1, 4))
 print("\nClassification Report:\n")
 print(classification_report(y_test, y_pred))
 
-# -----------------------------
-# SAVE OUTPUTS
-# -----------------------------
 os.makedirs("outputs", exist_ok=True)
 
-# Save model
 joblib.dump(model, "outputs/model.pkl")
 
-# Save test data
 pd.DataFrame(X_test).to_csv("outputs/X_test.csv", index=False)
 pd.DataFrame(y_test).to_csv("outputs/y_test.csv", index=False)
 
